438b11b6844909323fe80b7ed668c3f322468333
allan716 <525223688@qq.com>
2022-10-31 09:47:39 +0800

    新增，多开浏览器功能
    Signed-off-by: allan716 <525223688@qq.com>

---
+++ multi_browser_test.go
@@ -0,0 +1,25 @@
+package rod_helper
+
+import (
+	"github.com/WQGroup/logger"
+	"testing"
+	"time"
+)
+
+func TestNewMultiBrowser(t *testing.T) {
+
+	browserOptions := NewBrowserOptions(logger.GetLogger(), true)
+	browserOptions.SetXrayPoolUrl("192.168.50.233")
+	browserOptions.SetXrayPoolPort("19038")
+	b := NewMultiBrowser(browserOptions)
+
+	for i := 0; i < 5; i++ {
+		page, _, err := NewPageNavigateWithProxy(b.GetLBBrowser(), b.LbHttpUrl, "https://www.ipaddress.my/", 10*time.Second)
+		if err != nil {
+			return
+		}
+		page.Close()
+	}
+
+	println(b)
+}
