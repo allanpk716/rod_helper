c553c3fed74162368257ea81fd8c370e75be5923
allan716 <525223688@qq.com>
2022-11-05 10:02:31 +0800

    调整细节，新增获取 xray pool 开启的所有代理端口的设置
    Signed-off-by: allan716 <525223688@qq.com>

--- multi_browser.go
+++ multi_browser.go
@@ -16,16 +16,15 @@ import (
 
 type Browser struct {
 	log             *logrus.Logger
-	rodOptions      *BrowserOptions // 参数
-	multiBrowser    []*rod.Browser  // 多浏览器实例
-	browserIndex    int             // 当前使用的浏览器的索引
-	browserLocker   sync.Mutex      // 浏览器的锁
-	httpProxyIndex  int             // 当前使用的 http 代理的索引
-	httpProxyLocker sync.Mutex      // http 代理的锁
-	LbHttpUrl       string          // 负载均衡的 http proxy url
-	LBPort          int             //负载均衡 http 端口
-	httpProxyUrls   []string        // XrayPool 中的代理信息
-	socksProxyUrls  []string        // XrayPool 中的代理信息
+	rodOptions      *BrowserOptions     // 参数
+	multiBrowser    []*rod.Browser      // 多浏览器实例
+	browserIndex    int                 // 当前使用的浏览器的索引
+	browserLocker   sync.Mutex          // 浏览器的锁
+	httpProxyIndex  int                 // 当前使用的 http 代理的索引
+	httpProxyLocker sync.Mutex          // http 代理的锁
+	LbHttpUrl       string              // 负载均衡的 http proxy url
+	LBPort          int                 //负载均衡 http 端口
+	proxyInfos      []XrayPoolProxyInfo // XrayPool 中的代理信息
 }
 
 // NewMultiBrowser 面向与爬虫的时候使用 Browser
@@ -69,11 +68,18 @@ func NewMultiBrowser(browserOptions *BrowserOptions) *Browser {
 		log:          browserOptions.Log,
 		rodOptions:   browserOptions,
 		multiBrowser: make([]*rod.Browser, 0),
+		proxyInfos:   make([]XrayPoolProxyInfo, 0),
 	}
 
 	for _, result := range proxyResult.OpenResultList {
-		b.httpProxyUrls = append(b.httpProxyUrls, httpPrefix+browserOptions.XrayPoolUrl()+":"+strconv.Itoa(result.HttpPort))
-		b.socksProxyUrls = append(b.socksProxyUrls, socksPrefix+browserOptions.XrayPoolUrl()+":"+strconv.Itoa(result.SocksPort))
+
+		tmpProxyInfos := XrayPoolProxyInfo{
+			Name:       result.Name,
+			ProtoModel: result.ProtoModel,
+			HttpUrl:    httpPrefix + browserOptions.XrayPoolUrl() + ":" + strconv.Itoa(result.HttpPort),
+			SocksUrl:   socksPrefix + browserOptions.XrayPoolUrl() + ":" + strconv.Itoa(result.SocksPort),
+		}
+		b.proxyInfos = append(b.proxyInfos, tmpProxyInfos)
 	}
 	b.LBPort = proxyResult.LBPort
 
@@ -112,6 +118,24 @@ func (b *Browser) GetLBBrowser() *rod.Browser {
 	return b.multiBrowser[b.browserIndex]
 }
 
+func (b *Browser) GetOneProxyInfo() (XrayPoolProxyInfo, error) {
+	b.httpProxyLocker.Lock()
+	defer func() {
+		b.httpProxyIndex++
+		b.httpProxyLocker.Unlock()
+	}()
+
+	if len(b.proxyInfos) < 1 {
+		return XrayPoolProxyInfo{}, errors.New("proxyInfos is empty")
+	}
+
+	if b.httpProxyIndex >= len(b.proxyInfos) {
+		b.httpProxyIndex = 0
+	}
+
+	return b.proxyInfos[b.httpProxyIndex], nil
+}
+
 // NewBrowser 每次新建一个 Browser ，使用 HttpProxy 列表中的一个作为代理
 func (b *Browser) NewBrowser() (*rod.Browser, error) {
 
@@ -121,11 +145,15 @@ func (b *Browser) NewBrowser() (*rod.Browser, error) {
 		b.httpProxyLocker.Unlock()
 	}()
 
-	if b.httpProxyIndex >= len(b.httpProxyUrls) {
+	if len(b.proxyInfos) < 1 {
+		return nil, errors.New("proxyInfos is empty")
+	}
+
+	if b.httpProxyIndex >= len(b.proxyInfos) {
 		b.httpProxyIndex = 0
 	}
 
-	oneBrowser, err := NewBrowserBase(b.rodOptions.BrowserFPath(), b.httpProxyUrls[b.httpProxyIndex], b.rodOptions.LoadAdblock())
+	oneBrowser, err := NewBrowserBase(b.rodOptions.BrowserFPath(), b.proxyInfos[b.httpProxyIndex].HttpUrl, b.rodOptions.LoadAdblock())
 	if err != nil {
 		return nil, errors.New("NewBrowser.NewBrowserBase error:" + err.Error())
 	}
@@ -154,6 +182,13 @@ type OpenResult struct {
 	ProtoModel string `json:"proto_model"`
 	SocksPort  int    `json:"socks_port"`
 	HttpPort   int    `json:"http_port"`
+}
+
+type XrayPoolProxyInfo struct {
+	Name       string `json:"name"`
+	ProtoModel string `json:"proto_model"`
+	SocksUrl   string `json:"socks_url"`
+	HttpUrl    string `json:"http_url"`
 }
 
 const (
