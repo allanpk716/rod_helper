46a743f69299cfb81647d0b15a26f9949bfce0ef
allan716 <525223688@qq.com>
2022-12-18 11:24:53 +0800

    重构 http client 的配置逻辑
    Signed-off-by: allan716 <525223688@qq.com>

--- http_client.go
+++ http_client.go
@@ -4,13 +4,12 @@ import (
 	"crypto/tls"
 	"github.com/go-resty/resty/v2"
 	"net/http"
+	"net/url"
 	"time"
 )
 
 // NewHttpClient 新建一个 resty 的对象
 func NewHttpClient(opt *HttpClientOptions) (*resty.Client, error) {
-	//const defUserAgent = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50"
-	//const defUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.864.41"
 
 	var UserAgent string
 	// ------------------------------------------------
@@ -22,12 +21,12 @@ func NewHttpClient(opt *HttpClientOptions) (*resty.Client, error) {
 		MaxIdleConns:        1000,
 		MaxIdleConnsPerHost: 1000,
 	})
-	httpClient.SetTimeout(opt.HTMLTimeOut)
+	httpClient.SetTimeout(opt.htmlTimeOut)
 	httpClient.SetRetryCount(1)
 	// ------------------------------------------------
 	// 设置 Referer
-	if len(opt.Referer) > 0 {
-		httpClient.SetHeader("Referer", opt.Referer)
+	if len(opt.Referer()) > 0 {
+		httpClient.SetHeader("Referer", opt.Referer())
 	}
 	// ------------------------------------------------
 	// 设置 Header
@@ -39,10 +38,22 @@ func NewHttpClient(opt *HttpClientOptions) (*resty.Client, error) {
 	// 不要求安全链接
 	httpClient.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
 	// ------------------------------------------------
-	// http 代理
-	if opt.HttpProxyUrl != "" {
-		httpClient.SetProxy(opt.HttpProxyUrl)
-	} else {
+	proxyType, proxyUrl := opt.ProxyUrl()
+	switch proxyType {
+	case Http:
+		httpClient.SetProxy(proxyUrl)
+	case Socks5:
+		proxy := func(_ *http.Request) (*url.URL, error) {
+			return url.Parse(proxyUrl)
+		}
+		httpTransport := &http.Transport{
+			Proxy:               proxy,
+			DisableKeepAlives:   true,
+			MaxIdleConns:        1000,
+			MaxIdleConnsPerHost: 1000,
+		}
+		httpClient.SetTransport(httpTransport)
+	default:
 		httpClient.RemoveProxy()
 	}
 
@@ -50,15 +61,59 @@ func NewHttpClient(opt *HttpClientOptions) (*resty.Client, error) {
 }
 
 type HttpClientOptions struct {
-	HTMLTimeOut  time.Duration
-	HttpProxyUrl string
-	Referer      string
+	htmlTimeOut    time.Duration
+	proxyType      ProxyType
+	httpProxyUrl   string
+	socks5ProxyUrl string
+	referer        string
 }
 
-func NewHttpClientOptions(HTMLTimeOut time.Duration, httpProxyUrl string, referer string) *HttpClientOptions {
+func NewHttpClientOptions(HTMLTimeOut time.Duration) *HttpClientOptions {
 	return &HttpClientOptions{
-		HTMLTimeOut:  HTMLTimeOut,
-		HttpProxyUrl: httpProxyUrl,
-		Referer:      referer,
+		htmlTimeOut: HTMLTimeOut,
 	}
 }
+
+func (h *HttpClientOptions) HtmlTimeOut() time.Duration {
+	return h.htmlTimeOut
+}
+
+// SetHttpProxy 输入这样的代理连接：http://127.0.0.1:9150
+func (h *HttpClientOptions) SetHttpProxy(SetHttpProxy string) {
+	h.proxyType = Http
+	h.httpProxyUrl = SetHttpProxy
+}
+
+// SetSocks5Proxy 输入这样的代理连接：socks5://127.0.0.1:9150
+func (h *HttpClientOptions) SetSocks5Proxy(SetSock5Proxy string) {
+	h.proxyType = Socks5
+	h.socks5ProxyUrl = SetSock5Proxy
+}
+
+func (h *HttpClientOptions) ProxyUrl() (ProxyType, string) {
+	switch h.proxyType {
+	case None:
+		return None, ""
+	case Http:
+		return Http, h.httpProxyUrl
+	case Socks5:
+		return Socks5, h.socks5ProxyUrl
+	}
+	return None, ""
+}
+
+func (h *HttpClientOptions) SetReferer(referer string) {
+	h.referer = referer
+}
+
+func (h *HttpClientOptions) Referer() string {
+	return h.referer
+}
+
+type ProxyType int
+
+const (
+	None ProxyType = iota + 1
+	Http
+	Socks5
+)
